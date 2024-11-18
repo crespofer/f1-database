f23d633f6f55dbd62d4cfc54d505c53b4a390fe3
Fernando <crespofernando05@gmail.com>
2024-11-17 19:30:54 -0500

    add api call for circuits using user year

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -1,8 +1,15 @@
-import React, {useState, useEffect} from "react"
+import React, {useState, useEffect, useRef} from "react"
 import './HomeSearch.css'
 
 function HomeSearch(){
     const [chosenYear, setChosenYear] = useState();
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
+    const [circuits, setCircuits] = useState();
+    const [chosenCircuitId, setChosenCircuitId] = useState();
+
+    const abortControllerRef = useRef(null);
+    const BASE_URL = `https://ergast.com/api/f1/${chosenYear}`;
 
     var yearsList = []
     for(let i = 2023;i>=1950;i--) // populates array with list of valid years(1950-2023)
@@ -12,16 +19,61 @@ function HomeSearch(){
         setChosenYear(event.target.value);
     }
 
+    function whatever(){
+        console.log(circuits.MRData.CircuitTable.Circuits);
+    }
+
+    useEffect(() => {
+        if(!chosenYear) return;
+        
+        const fetchCircuits = async () => {
+            abortControllerRef.current?.abort(); // aborts if a controller exists
+            abortControllerRef.current = new AbortController(); // sets curr to new controller
+
+            setIsLoading(true);
+
+            try{
+                const response = await fetch(`${BASE_URL}/circuits.json`, {
+                    signal: abortControllerRef.current?.signal,
+                });
+                const circuits = await response.json();
+                setCircuits(circuits);
+            } catch (e) {
+                if(e.name === "AbortError"){
+                    console.log("Aborted");
+                    return;
+                }
+
+                setError(e);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchCircuits();
+    }, [chosenYear]);
+
+    if(isLoading)
+    {
+        return <div>Is loading...</div>
+    }
+
+    if(error)
+    {
+        console.log(error);
+    }
+
     return (
         <div className="search-container">
             <p className="title">Search for a Race!</p>
             <p className="description">Enter a year and Circuit</p>
             <select value={chosenYear} onChange={handleYearChange}>
-                <option>Select a year</option>
+                <option key={0} value="">Select a year</option>
                 {yearsList.map(year => (
                     <option key={year} value={year}>{year}</option>
                 ))}
             </select>
+            <button onClick={whatever}>Test test test</button>
         </div>
     );
 
