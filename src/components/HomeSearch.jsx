f944e9a031ef22b75cf8a8ee2028f6803489d9c5
Fernando <crespofernando05@gmail.com>
2024-11-19 01:51:22 -0500

    set up search screen

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -1,12 +1,16 @@
 import React, {useState, useEffect, useRef} from "react"
 import './HomeSearch.css'
+import loadingGif from '../assets/loading.gif'
 
 function HomeSearch(){
     const [chosenYear, setChosenYear] = useState();
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState();
-    const [circuits, setCircuits] = useState();
-    const [chosenCircuitId, setChosenCircuitId] = useState();
+    const [circuits, setCircuits] = useState([]);
+    const [chosenCircuitId, setChosenCircuitId] = useState("");
+    const [isYearSelected, setIsYearSelected] = useState(false);
+    const [isCircuitSelected, setIsCircuitSelected] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(false);
 
     const abortControllerRef = useRef(null);
     const BASE_URL = `https://ergast.com/api/f1/${chosenYear}`;
@@ -17,10 +21,37 @@ function HomeSearch(){
 
     function handleYearChange(event){
         setChosenYear(event.target.value);
+
+        if(event.target.value === "")
+        {
+            setIsYearSelected(false);
+            setChosenCircuitId("");
+        }
+        else{
+            setIsYearSelected(true);
+        }
     }
 
-    function whatever(){
-        console.log(circuits.MRData.CircuitTable.Circuits);
+    function handleChosenCircuitChange(event){
+        setChosenCircuitId(event.target.value);
+
+        if(event.target.value === ""){
+            setIsCircuitSelected(false);
+        }
+        else{
+            setIsCircuitSelected(true);
+        }
+    }
+
+    function search(){
+        if(!isYearSelected || !isCircuitSelected){
+            setErrorMessage(true);
+            return;
+        }
+        else{
+            setErrorMessage(false);
+        }
+        
     }
 
     useEffect(() => {
@@ -37,7 +68,7 @@ function HomeSearch(){
                     signal: abortControllerRef.current?.signal,
                 });
                 const circuits = await response.json();
-                setCircuits(circuits);
+                setCircuits(circuits.MRData.CircuitTable.Circuits);
             } catch (e) {
                 if(e.name === "AbortError"){
                     console.log("Aborted");
@@ -53,27 +84,38 @@ function HomeSearch(){
         fetchCircuits();
     }, [chosenYear]);
 
-    if(isLoading)
-    {
-        return <div>Is loading...</div>
-    }
-
     if(error)
     {
         console.log(error);
+        return <div className="error">
+            An Error Occured: Please Try Again
+            <button onClick={() => window.location.reload(false)}>Refresh</button>
+        </div>
     }
 
     return (
         <div className="search-container">
             <p className="title">Search for a Race!</p>
             <p className="description">Enter a year and Circuit</p>
+            <div className="select-container">
             <select value={chosenYear} onChange={handleYearChange}>
                 <option key={0} value="">Select a year</option>
                 {yearsList.map(year => (
                     <option key={year} value={year}>{year}</option>
                 ))}
             </select>
-            <button onClick={whatever}>Test test test</button>
+            <select value={chosenCircuitId} onChange={handleChosenCircuitChange}>
+                <option key={-1} value="">Select a Circuit</option>
+                {isYearSelected && Array.isArray(circuits) && circuits.map(circuit => (
+                    <option key={circuit.circuitId} value={circuit.circuitId}>
+                        {circuit.circuitName}
+                    </option>
+                ))}
+            </select>
+            </div>
+            <button className="search-button" onClick={search}>Search</button>
+            {errorMessage && <p className="error-message">Please select year and circuit</p>}
+            {isLoading && <img className="loading" src={loadingGif} alt=""/>}
         </div>
     );
 
